47c2f480d065ce969d8b17707a1481260929445e
Rafael <rafael_guliev94@list.ru>
2020-02-02 21:25:37 +0300

    master | добавляет шрифты

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import styles from './Header.scss';
+import logo from './img/logo.svg';
+
+export default class Header extends Component {
+    render() {
+        return (
+            <header className={styles.header}>
+                <img src={logo} alt=""/>
+                hello world
+            </header>
+        );
+    }
+}
