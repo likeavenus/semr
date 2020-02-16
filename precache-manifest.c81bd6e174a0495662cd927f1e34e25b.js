cabaa30b9a544506932aceade3f137aef5a9e9f7
Rafael <rafael_guliev94@list.ru>
2020-02-16 14:30:08 +0300

    Updates

---
+++ precache-manifest.c81bd6e174a0495662cd927f1e34e25b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d02c92ba001ecb2d167b6c6f195a6bab",
+    "url": "/semr/index.html"
+  },
+  {
+    "revision": "31c60acec636714ebf58",
+    "url": "/semr/static/css/main.c755527a.chunk.css"
+  },
+  {
+    "revision": "f258893df13ae2396bba",
+    "url": "/semr/static/js/2.aa1b4881.chunk.js"
+  },
+  {
+    "revision": "31c60acec636714ebf58",
+    "url": "/semr/static/js/main.c2c57396.chunk.js"
+  },
+  {
+    "revision": "9c754b7e18192c11e635",
+    "url": "/semr/static/js/runtime-main.bdf92e1d.js"
+  },
+  {
+    "revision": "278321edc4a28fc3f2441b1472e9347a",
+    "url": "/semr/static/media/Ubuntu-Bold.278321ed.woff"
+  },
+  {
+    "revision": "c636b9b677fbd28b6596bf4ff1043f44",
+    "url": "/semr/static/media/Ubuntu-Bold.c636b9b6.woff2"
+  },
+  {
+    "revision": "37fefefcfd1a1e4dd4b05c2125ed78cf",
+    "url": "/semr/static/media/Ubuntu-Medium.37fefefc.woff"
+  },
+  {
+    "revision": "51496ddf1ce33c063212ff1323c0e7b7",
+    "url": "/semr/static/media/Ubuntu-Medium.51496ddf.woff2"
+  },
+  {
+    "revision": "b7350427f5d9fbceabb3325336d69531",
+    "url": "/semr/static/media/Ubuntu-Regular.b7350427.woff2"
+  },
+  {
+    "revision": "ba4e7809d474b7b6936d09346695c3ae",
+    "url": "/semr/static/media/Ubuntu-Regular.ba4e7809.woff"
+  },
+  {
+    "revision": "efd54ad6e63de91a3a175a17e9bbc859",
+    "url": "/semr/static/media/bg.efd54ad6.jpg"
+  },
+  {
+    "revision": "0221075ee40286d81ea016ed11e9e233",
+    "url": "/semr/static/media/close.0221075e.svg"
+  },
+  {
+    "revision": "81d7f444b923b85183e2453549e4e63c",
+    "url": "/semr/static/media/logo.81d7f444.svg"
+  }
+]);
